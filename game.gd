fc098e2972695df36dfd8524b128b3b6d34f4992
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2024-02-04 15:27:56 -0300

    adicionando código do jogo

---
+++ game.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+func _process(delta):
+	$Label.text = str(Main.p1_score)
+	$Label2.text = str(Main.p2_score)
+	
+func _on_top_body_entered(body):
+	body.direction.y *= -1
+
+func _on_bottom_body_entered(body):
+	body.direction.y *= -1
+
+func _on_left_body_entered(body):
+	body.queue_free()
+	var e = preload("res://ball.tscn").instantiate()
+	e.global_position = Vector2(588, 327)
+	add_child(e)
+	Main.p2_score += 1
+
+func _on_rigtht_body_entered(body):
+	body.queue_free()
+	var e = preload("res://ball.tscn").instantiate()
+	e.global_position = Vector2(588, 327)
+	add_child(e)
+	Main.p1_score += 1
